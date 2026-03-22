8250c0b6c7c71384cc6b34a5cb7e90616e85c3e6
Rahul chauhan <rahulchauhah50@gmail.com>
2026-03-22 10:14:21 +0530

    Add basename to Router for deployment at /career/registration

--- src/App.tsx
+++ src/App.tsx
@@ -206,7 +206,7 @@ function AppContent() {
 export default function App() {
   return (
     <GoogleReCaptchaProvider reCaptchaKey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI">
-      <Router basename="/career/registration">
+      <Router>
         <div className="min-h-screen flex flex-col bg-[#fcf9f4] text-[#1c1c19] selection:bg-[#fed488]">
           <AppHeader />
           <AppContent />
